6c8df8017767d68c05c7b71b227aefdf2ac192bd
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-05-07 16:06:53 +0100

    event handlers passed to child components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,54 +1,23 @@
 import { useState } from "react";
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>The app is used by pressing the buttons</div>;
-  }
-  return <div>Button press history: {props.allClicks.join(" ")}</div>;
-};
 
-const Button = (props) => {
-  const { onClick, text } = props;
-  return <button onClick={onClick}>{text}</button>;
-};
+const Display = (props) => <div>{props.value}</div>;
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  const [value, setValue] = useState(10);
 
-  const [allClicks, setAll] = useState([]);
-  const [total, setTotal] = useState(0);
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    setTotal(updatedLeft + right);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setTotal(left + updatedRight);
-  };
-  const clearCount = () => {
-    setAll([]);
-    setTotal(0);
-    setLeft(0);
-    setRight(0);
+  const setToValue = (newValue) => {
+    console.log("value now", newValue);
+    setValue(newValue);
   };
 
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text="Left" />
-      <Button onClick={handleRightClick} text="Right" />
-      {right}
-      <History allClicks={allClicks} />
-      <p>Total: {total}</p>
-      <p>
-        <Button onClick={clearCount} text="Clear" />
-      </p>
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="button" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
