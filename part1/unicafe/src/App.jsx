614f613ceacd888b63c49bfb577aef1c30af64e5
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-05-07 19:23:15 +0100

    feedback buttons and total counters rendered

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    const updatedGood = good + 1;
+    setTotal(updatedGood + neutral + bad);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    const updatedNeutral = neutral + 1;
+    setTotal(updatedNeutral + good + bad);
+  };
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    const updatedBad = bad + 1;
+    setTotal(updatedBad + neutral + good);
+  };
+
+  return (
+    <div>
+      <h1>Give your feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average:</p>
+      <p>Positive:</p>
+    </div>
+  );
+};
+
+export default App;
