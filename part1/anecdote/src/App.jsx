3ceae262c7254e03d8616bc5fab3dc310aaedfad
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-05-22 12:29:00 +0100

    removed redundant return statements

--- part1/anecdote/src/App.jsx
+++ part1/anecdote/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
     const copyVotes = [...votes];
     copyVotes[selected] += 1;
     setMostVotedAnecdote(findMaxIndex(copyVotes));
-    return setVotes(copyVotes);
+    setVotes(copyVotes);
   };
 
   const findMaxIndex = (votes) => {
