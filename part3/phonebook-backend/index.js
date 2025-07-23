6b5d03267525843031393494039254c946c79815
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-07-23 16:46:15 +0100

    info page added

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -34,6 +34,12 @@ app.get("/api/persons", (request, response) => {
   response.json(phonebook);
 });
 
+app.get("/info", (request, response) => {
+  const info = `<p>Phonebook has info for ${phonebook.length} people</p>
+    <p> ${new Date()}</p>`;
+  response.send(info);
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
