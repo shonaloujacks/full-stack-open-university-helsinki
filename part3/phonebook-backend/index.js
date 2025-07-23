618f2938a4276deea82be1e2eb563fa854fcfdf3
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-07-23 17:43:49 +0100

    set up delete functionality

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -40,6 +40,23 @@ app.get("/info", (request, response) => {
   response.send(info);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = phonebook.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  phonebook = phonebook.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
