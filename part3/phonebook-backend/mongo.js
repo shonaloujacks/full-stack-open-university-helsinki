1260c79823431ef83ef76e18597c0b9e47959312
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-08-06 11:16:10 +0100

    mongodb schema updated

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -7,25 +7,25 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://wordsbyshonajackson:<db_password>${password}@phonebook.rpvmrjp.mongodb.net/?retryWrites=true&w=majority&appName=Phonebook`;
+const url = `mongodb+srv://wordsbyshonajackson:${password}@phonebook.rpvmrjp.mongodb.net/?retryWrites=true&w=majority&appName=Phonebook`;
 
 mongoose.set("strictQuery", false);
 
 mongoose.connect(url);
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const phonebookEntrySchema = new mongoose.Schema({
+  name: String,
+  number: String,
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const PhonebookEntry = mongoose.model("PhonebookEntry", phonebookEntrySchema);
 
-const note = new Note({
-  content: "HTML is easy",
-  important: true,
+const person = new PhonebookEntry({
+  name: "Anna Michaels",
+  number: "045-123456",
 });
 
-note.save().then((result) => {
-  console.log("note saved!");
+person.save().then((result) => {
+  console.log(`added ${person.name} number ${person.number} to phonebook`);
   mongoose.connection.close();
 });
