7996a5baf4cf1e51b889b42803a94a6e06096d92
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-08-06 12:10:59 +0100

    conditional logic added

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -22,12 +22,21 @@ const phonebookEntrySchema = new mongoose.Schema({
 
 const PhonebookEntry = mongoose.model("PhonebookEntry", phonebookEntrySchema);
 
-const person = new PhonebookEntry({
-  name: name,
-  number: number,
-});
+if (process.argv.length > 3) {
+  const person = new PhonebookEntry({
+    name: name,
+    number: number,
+  });
 
-person.save().then((result) => {
-  console.log(`added ${person.name} number ${person.number} to phonebook`);
-  mongoose.connection.close();
-});
+  person.save().then((result) => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  PhonebookEntry.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+}
