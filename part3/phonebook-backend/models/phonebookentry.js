56ba180a5d1c262171260f83450ed3cd1bc6e610
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-08-08 16:31:21 +0100

    schema expanded

--- part3/phonebook-backend/models/phonebookentry.js
+++ part3/phonebook-backend/models/phonebookentry.js
@@ -18,7 +18,11 @@ console.log("URI: ", url);
 connectToMongoDB();
 
 const phonebookEntrySchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
   number: String,
 });
 
