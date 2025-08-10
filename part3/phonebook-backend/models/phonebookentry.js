b4877dd8290c5e52a48b79e9a44a4dfc10d7ec23
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-08-10 13:23:39 +0100

    custom validator added to schema

--- part3/phonebook-backend/models/phonebookentry.js
+++ part3/phonebook-backend/models/phonebookentry.js
@@ -17,13 +17,35 @@ const connectToMongoDB = async () => {
 console.log("URI: ", url);
 connectToMongoDB();
 
+const numberValidator = (numberEntry) => {
+  console.log(phonebookEntrySchema);
+  if (numberEntry.split("-").length !== 2) {
+    throw new mongoose.Error.ValidationError("More than one hyphen");
+  }
+  const [numberPartOne, numberPartTwo] = numberEntry.split("-");
+  if (isNaN(numberPartOne || numberPartTwo) === true) {
+    throw new mongoose.Error.ValidationError(
+      "Phone number contains non numerical characters"
+    );
+  }
+  if (numberPartOne.length !== 2 || 3) {
+    throw new mongoose.Error.ValidationError(
+      "First part of phone number must include 2-3 digits"
+    );
+  }
+};
+
 const phonebookEntrySchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: numberValidator,
+  },
 });
 
 phonebookEntrySchema.set("toJSON", {
