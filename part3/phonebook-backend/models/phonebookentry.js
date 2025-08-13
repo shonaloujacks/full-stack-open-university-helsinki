53d871ab3eccee5ff167b066fe1c6bf04d941f38
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-08-13 12:14:29 +0100

    eslint configurated and number length logic corrected

--- part3/phonebook-backend/models/phonebookentry.js
+++ part3/phonebook-backend/models/phonebookentry.js
@@ -1,39 +1,39 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(url);
-    console.log("connected to MongoDB");
+    await mongoose.connect(url)
+    console.log('connected to MongoDB')
   } catch (error) {
-    console.log("error connecting to MongoDB", error.message);
+    console.log('error connecting to MongoDB', error.message)
   }
-};
-console.log("URI: ", url);
-connectToMongoDB();
+}
+console.log('URI: ', url)
+connectToMongoDB()
 
 const numberValidator = (numberEntry) => {
-  console.log(phonebookEntrySchema);
-  if (numberEntry.split("-").length !== 2) {
-    throw new mongoose.Error.ValidationError("More than one hyphen");
+  console.log(phonebookEntrySchema)
+  if (numberEntry.split('-').length !== 2) {
+    throw new mongoose.Error.ValidationError('More than one hyphen')
   }
-  const [numberPartOne, numberPartTwo] = numberEntry.split("-");
+  const [numberPartOne, numberPartTwo] = numberEntry.split('-')
   if (isNaN(numberPartOne || numberPartTwo) === true) {
     throw new mongoose.Error.ValidationError(
-      "Phone number contains non numerical characters"
-    );
+      'Phone number contains non numerical characters'
+    )
   }
-  if (numberPartOne.length !== 2 || 3) {
+  if (numberPartOne.length !== 2 && numberPartOne.length !== 3) {
     throw new mongoose.Error.ValidationError(
-      "First part of phone number must include 2-3 digits"
-    );
+      'First part of phone number must include 2-3 digits'
+    )
   }
-};
+}
 
 const phonebookEntrySchema = new mongoose.Schema({
   name: {
@@ -46,14 +46,14 @@ const phonebookEntrySchema = new mongoose.Schema({
     minLength: 8,
     validate: numberValidator,
   },
-});
+})
 
-phonebookEntrySchema.set("toJSON", {
+phonebookEntrySchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("PhonebookEntry", phonebookEntrySchema);
+module.exports = mongoose.model('PhonebookEntry', phonebookEntrySchema)
