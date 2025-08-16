4281873573f916c94e9f70bb9e985e6a22c9ac16
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-08-16 19:11:06 +0100

    most likes function and test added

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -30,13 +30,38 @@ const mostBlogs = (blogs) => {
   else {
     const authorList = blogs.map((blog) => blog.author)
     let authorFrequency = _.countBy(authorList)
-    let maxAuthor = _.maxBy(Object.keys(authorFrequency),o => authorFrequency[o] )
+    console.log('THIS IS AUTHORFREQUENCY', authorFrequency)
+    // counts number of each unique instance and turns into object that maps author name to count
+    let maxAuthor = _.maxBy(Object.keys(authorFrequency), author => authorFrequency[author] )
+    // Object.keys(authorFrequency) produces an array of authors, then applies a function to each element that counts its instances. Then the max value (ie most frequent author) is returned
+    console.log('THIS IS MAXAUTHOR', maxAuthor)
     const maxCount = authorFrequency[maxAuthor]
+    //looks up maxAuthor name in the authorFrequency object, which returns the value of that corresponding name key
     console.log('value:', maxAuthor, 'count:', maxCount)
     return { value: maxAuthor, count: maxCount }
   }}
 
+const mostLikes = (blogs) => {
+  const authorLikes = {}
+  if (blogs === 0) {
+    return 0
+  }
+  blogs.forEach((blog) => {
+    authorLikes[blog.author] = (authorLikes[blog.author] ?? 0) + blog.likes
+    console.log('THIS IS AUTHOR LIKES:', authorLikes)
+  })
+
+  let maxKey
+  let highestLikes = 0
+  for (const [key, value] of Object.entries(authorLikes)) {
+    if (value > highestLikes) {
+      highestLikes = value
+      maxKey = key
+    }
+  }
+  return { author: maxKey, likes: highestLikes }
+}
 
 module.exports = {
-  dummy, totalLikes, favouriteBlog, mostBlogs
+  dummy, totalLikes, favouriteBlog, mostBlogs, mostLikes
 }
