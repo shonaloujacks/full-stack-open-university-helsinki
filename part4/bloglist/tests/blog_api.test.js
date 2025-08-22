8aec192cb7143c9cc52906218c5a60e91f5dad69
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-08-22 10:43:58 +0100

    beforeEach added to tests

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,14 +1,36 @@
 const assert = require('node:assert')
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
+const initialBlogs = [
+  {
+    title: 'Courgette & lemon risotto',
+    author: 'Chelsie Collins',
+    url: 'https://www.bbcgoodfood.com/recipes/courgette-lemon-risotto',
+    likes: 5
+  },
+
+  {
+    title: 'Jerusalem artichoke fritters',
+    author: 'Marie Mitchell',
+    url: 'https://www.theguardian.com/food/2024/dec/02/marie-mitchells-jerusalem-artichoke-fritters-with-clementine-sauce-recipe',
+    likes: 4
+  }
+]
+
+beforeEach(async() => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
 test.only('notes are returned as json', async () => {
   const response = await api
-    .get('/api/notes')
+    .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
