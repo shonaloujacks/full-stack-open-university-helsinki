e84e4073f41221ab827b72ca214e7fc80b8df9b2
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-09-06 14:36:09 +0100

    user tests added

--- part4/bloglist/tests/test_helper.js
+++ part4/bloglist/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -22,7 +23,13 @@ const blogsInDB = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDB = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
+}
+
 module.exports = {
   initialBlogs,
-  blogsInDB
+  blogsInDB,
+  usersInDB
 }
