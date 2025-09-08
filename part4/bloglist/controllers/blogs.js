c93b93fa4a78d523ac948f9dda4d7af6bb86ced9
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-09-08 10:55:04 +0100

    user info added to blog objects

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -5,7 +5,7 @@ const User = require('../models/user')
 
 blogRouter.get('/', async (request, response, next) => {
   try {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
   }
   catch (error) {
