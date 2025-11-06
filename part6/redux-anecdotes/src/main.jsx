860e2756aa7e99ef714c55c65a6692aedd880e18
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-11-06 12:27:38 +0000

    Redux Toolkit installed and new store file created

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,19 +1,11 @@
 import ReactDOM from 'react-dom/client'
-import { combineReducers, legacy_createStore as createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App'
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
+import store from './store'
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer
-})
 
-const store = createStore(reducer)
 
-console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
