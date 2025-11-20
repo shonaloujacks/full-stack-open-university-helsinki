43c3cebb2337b02cd468794ce906d9bd518a753d
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-11-20 14:55:44 +0000

    logic to retrieve anecdotes from server implemented

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,18 +1,34 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
 
 const App = () => {
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ]
+  
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3001/anecdotes')
+      if (!response.ok) {
+        throw new Error('Failed to fetch notes')
+      }
+      return await response.json()
+    }
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+  if (result.isError) {
+    return <span>Anecdote service not available due to problems with server</span>
+  }
+  
+  const anecdotes = result.data
 
   return (
     <div>
--- part6/query-anecdotes/src/main.jsx
+++ part6/query-anecdotes/src/main.jsx
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+  
 )
