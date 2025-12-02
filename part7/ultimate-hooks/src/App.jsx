c5e7e5926e6b6c33700111be3d049e674b0487e8
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-12-02 14:25:03 +0000

    useResource hook logic added

--- part7/ultimate-hooks/src/App.jsx
+++ part7/ultimate-hooks/src/App.jsx
@@ -18,10 +18,17 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect (() => {
+    const getData = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
+    getData()
+  }, [baseUrl])
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
   }
 
   const service = {
