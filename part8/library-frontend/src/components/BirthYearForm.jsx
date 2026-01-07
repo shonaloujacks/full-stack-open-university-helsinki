7a79259ed88f8725a17576ee643c84d4d1fb0ec0
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2026-01-07 15:42:37 +0000

    birth year drop down implemented

--- part8/library-frontend/src/components/BirthYearForm.jsx
+++ part8/library-frontend/src/components/BirthYearForm.jsx
@@ -1,4 +1,4 @@
-import { useMutation } from "@apollo/client/react";
+import { useMutation, useQuery } from "@apollo/client/react";
 import { useState } from "react";
 import { ALL_AUTHORS, EDIT_BIRTH_YEAR } from "../queries";
 
@@ -12,6 +12,20 @@ const BirthYearForm = () => {
       console.log("THIS IS NEW BIRTH YEAR DATA", data.editAuthor);
     },
   });
+
+  const { data, loading } = useQuery(ALL_AUTHORS);
+
+  const authors = data?.allAuthors;
+
+  if (loading) {
+    return <div>Loading authors...</div>;
+  }
+
+  const authorsWithNoBirthYear = authors.filter(
+    (author) => author.born === null,
+  );
+
+  console.log("Authors with no birth year", authorsWithNoBirthYear);
 
   const submit = async (event) => {
     event.preventDefault();
@@ -28,11 +42,26 @@ const BirthYearForm = () => {
 
       <form onSubmit={submit}>
         <div>
-          name{" "}
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
+          <label>
+            name{" "}
+            <select
+              name="name"
+              value={name}
+              onChange={(event) => {
+                console.log("THIS IS E TARGET VALUE", event.target.value);
+                setName(event.target.value);
+              }}
+            >
+              <option value="" disabled>
+                Select an author
+              </option>
+              {authorsWithNoBirthYear.map((author) => (
+                <option key={author.id} value={author.name}>
+                  {author.name}
+                </option>
+              ))}
+            </select>
+          </label>
         </div>
         <div>
           born{""}
