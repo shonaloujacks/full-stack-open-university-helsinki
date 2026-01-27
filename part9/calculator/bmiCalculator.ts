e2675a39799d4a6c42bf0fecfcf461aa8743091c
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2026-01-27 14:10:04 +0000

    return type added to calculateBmi

--- part9/calculator/bmiCalculator.ts
+++ part9/calculator/bmiCalculator.ts
@@ -1,9 +1,8 @@
 console.log('PROCESS.ARV', process.argv) 
 
 interface BmiValues {
-  value1: number;
-  value2: number;
-
+  height: number;
+  weight: number;
 }
 
 
@@ -14,8 +13,8 @@ const parseArguments = (args: string[]): BmiValues => {
 
   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
     return {
-      value1: Number(args[2]),
-      value2: Number(args[3])
+      height: Number(args[2]),
+      weight: Number(args[3])
     }
   } else {
     throw new Error('Provided values were not numbers!');
@@ -23,7 +22,7 @@ const parseArguments = (args: string[]): BmiValues => {
 
 }
 
-const calculateBmi = (height: number, weight: number, printText: string) => {
+const calculateBmi = (height: number, weight: number, printText: string) : void => {
   const bmiResult = (weight / (height * height)) * 10000
   console.log('THIS IS BMI result', bmiResult)
 
@@ -31,23 +30,23 @@ const calculateBmi = (height: number, weight: number, printText: string) => {
    console.log(printText, 'Underweight')
   }
   
-  if (bmiResult > 18.5 && bmiResult < 25 ) {
+  if (bmiResult >= 18.5 && bmiResult < 25 ) {
     console.log(printText, 'Healthy range')
   }
 
-   if (bmiResult > 25 && bmiResult < 30 ) {
+   if (bmiResult >= 25 && bmiResult < 30 ) {
     console.log(printText, 'Overweight')
   }
 
-   if (bmiResult > 30 ) {
+   if (bmiResult >= 30 ) {
     console.log(printText, 'Obese')
   }
   
 }
 
 try {
-  const { value1, value2 } = parseArguments(process.argv);
-  calculateBmi(value1, value2, `Based on your weight of ${value1}kg and height of ${value2}cm, your BMI result is:`);
+  const { height, weight } = parseArguments(process.argv);
+  calculateBmi(height, weight, `Based on your weight of ${weight}kg and height of ${height}cm, your BMI result is:`);
 } catch (error: unknown) {
   let errorMessage = 'Something bad happened.'
   if (error instanceof Error) {
