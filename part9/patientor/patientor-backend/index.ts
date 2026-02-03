4df3a10872a549320b29d7073a79b9e678172840
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2026-02-03 09:56:24 +0000

    cors origin specified

--- part9/patientor/patientor-backend/index.ts
+++ part9/patientor/patientor-backend/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const cors = require('cors')
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({ origin: ['http://localhost:5174', 'http://127.0.0.1:5174']}))
 
 const PORT = 3000;
 
