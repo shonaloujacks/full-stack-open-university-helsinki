276a66ce20d13e622a60c0bcf2a72f30a776102d
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2026-02-16 14:23:44 +0000

    get request for individual patients implemented

--- part9/patientor/patientor-frontend/src/types.ts
+++ part9/patientor/patientor-frontend/src/types.ts
@@ -10,6 +10,10 @@ export enum Gender {
   Other = "other"
 }
 
+export interface Entry {
+
+}
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,6 +21,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
